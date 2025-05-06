65eaac38ec1e26ee73662c7e3c3be7a4b7bb0894
Arturo Sandoval <artssc925@gmail.com>
2025-05-05 18:37:12 -0600

    Primera versión final

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const products = JSON.parse(localStorage.getItem('Productos')) || [];
+let products = JSON.parse(localStorage.getItem('Productos')) || [];
 let productsContainer = document.getElementById('cardsContainer');
 
 let excard = document.getElementById('excard');
 let panel = document.getElementById('panel');
 let closeButton = document.getElementById('close');
 let crearProducto = document.getElementById('crearProducto');
-
+let borrarProductos = document.querySelector('.borrarProductos')
 
 excard.onclick = () => {
     panel.classList.add('mostrar-panel');
@@ -14,18 +14,25 @@ closeButton.onclick = () => {
     panel.classList.remove('mostrar-panel');
 }
 
+borrarProductos.onclick = () => {
+    products = []
+    localStorage.removeItem('Productos');
+    location.reload()
+}
 
 function mostrarProductos(productos){
     productos.forEach(producto => {
         const product = document.createElement("div");
         product.className = "card"
-        product.innerHTML = `<h5 class="card-title">${producto.nombreProducto}</h5>
+        product.innerHTML = `
+                            <h5 class="card-title">${producto.nombreProducto}</h5>
                           <p class="card-price">$${producto.precioProducto}</p>
                           <p class="card-desc">${producto.descProducto}</p>`
-        productsContainer.appendChild(product)
+        productsContainer.appendChild(product);
     })
 
 }
+
 
 crearProducto.onclick = () => {
     let nombreProducto = document.getElementById('nombre');
@@ -37,10 +44,13 @@ crearProducto.onclick = () => {
         precioProducto: precioProducto.value,
         descProducto: descProducto.value 
     })
-    localStorage.setItem("Productos", JSON.stringify(products))
+    localStorage.setItem("Productos", JSON.stringify(products));
     panel.classList.remove('mostrar-panel');
-    mostrarProductos(products);
-}
 
+    setTimeout(() => {
+        location.reload();
+    }, 100);
+    
+}
 
 mostrarProductos(products);
